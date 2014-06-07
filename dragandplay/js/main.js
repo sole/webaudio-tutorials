014be06b2d0277f7000f5a0e58631b815fdc1596
sole <sole@mozilla.com>
2014-06-07 21:47:43 +0100

    draw sample too

--- dragandplay/js/main.js
+++ dragandplay/js/main.js
@@ -4,6 +4,7 @@
 	
 
 	var canvas = document.querySelector('canvas');
+	var canvasContext = canvas.getContext('2d');
 
 	// audio setup
 	var audioContext = new AudioContext();
@@ -72,14 +73,15 @@
 		console.log('loadSample', arrayBuffer);
 		audioContext.decodeAudioData(arrayBuffer, function success(buffer) {
 			if(buffer) {
-				playSample(buffer);
+				useSample(buffer);
 			}
 		}, function fail(evt) {
 			console.error('Error loading sample', evt);
 		});
 	}
 
-	function playSample(buffer) {
+
+	function useSample(buffer) {
 		if(bufferSource) {
 			bufferSource.disconnect(audioContext.destination);
 		}
@@ -88,6 +90,48 @@
 		bufferSource.loop = true;
 		bufferSource.buffer = buffer;
 		bufferSource.start(0);
+
+		drawSample(buffer);
+	}
+
+
+	function drawSample(buffer) {
+		console.log(buffer);
+		var width = canvas.width;
+		var height = canvas.height;
+		var halfHeight = height * 0.5;
+		var bufferData = buffer.getChannelData(0);
+		var bufferLength = bufferData.length;
+
+		canvasContext.fillStyle = 'rgb(0, 0, 0)';
+		canvasContext.fillRect(0, 0, width, height);
+
+		canvasContext.lineWidth = 1;
+		canvasContext.strokeStyle = 'rgb(255, 0, 0)';
+
+		canvasContext.beginPath();
+
+		var sliceWidth = width * 1.0 / bufferLength;
+		var x = 0;
+
+
+		for(var i = 0; i < bufferLength; i++) {
+
+			var v = bufferData[i];
+			var y = (v + 1) * halfHeight;
+
+			if(i === 0) {
+				canvasContext.moveTo(x, y);
+			} else {
+				canvasContext.lineTo(x, y);
+			}
+
+			x += sliceWidth;
+		}
+
+		canvasContext.lineTo(width, halfHeight);
+
+		canvasContext.stroke();
 	}
 	
 
